fb8235c4093694c278a1df32da356cdbe46b3cd0
HeyD <hmhati@gmail.com>
2024-11-16 20:59:41 +0200

    Show tooltip if lapse is reduced

--- lapse_reducer/__init__.py
+++ lapse_reducer/__init__.py
@@ -1,6 +1,7 @@
 import anki.cards
 import aqt.reviewer
 
+from aqt import utils
 from aqt.gui_hooks import reviewer_did_answer_card
 
 def get_last_correct_answers(answers):
@@ -21,9 +22,10 @@ def was_consecutively_correct(card: anki.cards.Card, times: int):
 
 
 def on_answer(context: aqt.reviewer.Reviewer, card: anki.cards.Card, ease: int):
-    value = was_consecutively_correct(card, 2)
-    print(value)
-    print('User answered card')
+    updated_card = card.col.get_card(card.id)
+    if was_consecutively_correct(card, 1) and updated_card.lapses > 0:
+        updated_card.lapses -= 1
+        utils.tooltip('Reduced lapse of the card', period=3000)
 
 def init():
     reviewer_did_answer_card.append(on_answer)
