4ba4a61810644c183b353289d3488826f05a32ce
HeyD <hmhati@gmail.com>
2024-11-16 21:34:03 +0200

    Save new lapses

--- lapse_reducer/__init__.py
+++ lapse_reducer/__init__.py
@@ -6,7 +6,7 @@ from aqt.gui_hooks import reviewer_did_answer_card
 
 def get_last_correct_answers(answers):
     result = []
-    for element in reversed(answers):
+    for element in answers:
         if element > 1:
             result.append(element)
         else:
@@ -20,13 +20,14 @@ def was_consecutively_correct(card: anki.cards.Card, times: int):
 
     return total_consecutively_correct >= times
 
-
 def on_answer(context: aqt.reviewer.Reviewer, card: anki.cards.Card, ease: int):
     updated_card = card.col.get_card(card.id)
-    if was_consecutively_correct(card, 1) and updated_card.lapses > 0:
+    if was_consecutively_correct(card, 2) and updated_card.lapses > 0:
         updated_card.lapses -= 1
+        context.mw.col.update_card(updated_card)
+        context.mw.col.update_note(updated_card.note())
         utils.tooltip('Reduced lapse of the card', period=3000)
 
 def init():
-    reviewer_did_answer_card.append(on_answer)
     print('Setting up lapse reducer....')
+    reviewer_did_answer_card.append(on_answer)
