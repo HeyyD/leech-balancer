60d3d9dba49f8891532e8efa718de41a27ab4638
heidi.hati <heidi.hati@sulake.com>
2025-01-07 14:50:28 +0200

    Separate menu and main logic to files

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from .src import init
+from .main import init
+from .config import init_config
 
 init()
+init_config()
