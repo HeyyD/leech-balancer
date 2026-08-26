ed87009f52a63b458b7f4ffa4d4834143c2a38bd
HeyD <hmhati@gmail.com>
2026-08-26 17:25:04 +0300

    Remove unused files

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from .lapse_reducer import init
+from .src import init
 
 init()
